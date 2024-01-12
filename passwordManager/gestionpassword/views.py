1a1f8fc01d387d2afb3c253c9900a1be4b0803cd
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-01-12 11:14:36 +0100

    modification du design button

--- passwordManager/gestionpassword/views.py
+++ passwordManager/gestionpassword/views.py
@@ -13,7 +13,7 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login')
+            return redirect('home')
     else:
         form = UserCreationForm()
     return render(request, 'gestionpassword/signup.html', {'form': form})
