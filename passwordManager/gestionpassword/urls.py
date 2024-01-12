8ffd094611a43e80257921d64694bc0df9f60e5d
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-01-12 11:08:39 +0100

    config de la base.html

--- passwordManager/gestionpassword/urls.py
+++ passwordManager/gestionpassword/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('', views.home_view, name='home'),
+    path('add_password/', views.add_password_view, name='add_password'),
+    path('view_all/', views.view_all_passwords_view, name='view_all'),    
 ]
--- passwordManager/gestionpassword/views.py
+++ passwordManager/gestionpassword/views.py
@@ -17,3 +17,17 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'gestionpassword/signup.html', {'form': form})
+
+def home_view(request):
+    #logic home
+    return render(request, 'gestionpassword/home.html')
+
+
+def add_password_view(request):
+    #logic add password
+    return render(request, 'gestionpassword/add_password.html')
+
+
+def view_all_passwords_view(request):
+    #logic view all
+    return render(request, 'gestionpassword/view_all.html')
